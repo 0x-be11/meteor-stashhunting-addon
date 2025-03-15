90856cf24f6092601ad3b44d5b1086381e836257
miles352 <76878501+miles352@users.noreply.github.com>
2025-03-14 22:27:27 -0400

    Update AutoLogPlus.java

--- src/main/java/com/stash/hunt/modules/AutoLogPlus.java
+++ src/main/java/com/stash/hunt/modules/AutoLogPlus.java
@@ -136,7 +136,7 @@ public class AutoLogPlus extends Module
         }
         if (illegalDisconnect.get())
         {
-            mc.player.networkHandler.sendPacket(new PickItemFromEntityC2SPacket(-1, true));
+            mc.player.networkHandler.sendChatMessage(String.valueOf((char)0));
         }
         else
         {
