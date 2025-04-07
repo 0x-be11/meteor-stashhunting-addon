ccc1bbf8ef8bfd25b076af5abafadd87c22135dd
mtdaly <mtdaly@ncsu.edu>
2025-04-07 19:09:58 -0400

    test

--- src/main/java/com/stash/hunt/modules/GrimEfly.java
+++ src/main/java/com/stash/hunt/modules/GrimEfly.java
@@ -143,6 +143,7 @@ public class GrimEfly extends Module {
 
         if (bounce.get())
         {
+            info("Goal reset!");
             BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().setGoal(null);
         }
     }
@@ -182,6 +183,8 @@ public class GrimEfly extends Module {
             }
             else if (tempPath != null)
             {
+                info("Setting temp path to " + tempPath);
+                info("Current position is " + mc.player.getPos());
                 BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().setGoalAndPath(new GoalBlock(tempPath));
                 return;
             }
@@ -197,6 +200,7 @@ public class GrimEfly extends Module {
                 || mc.player.getY() > targetY.get() + 2
                 || mc.player.horizontalCollision))
             {
+                info("This shouldnt be at 0 0: " + mc.player.getPos());
                 paused.set(true);
                 double targetYaw = lockYaw.get() ? yaw.get() : mc.player.getYaw();
                 Vec3d pos;
@@ -213,6 +217,7 @@ public class GrimEfly extends Module {
                     if (assumeHighwayDirs.get())
                     {
                         Vec3d playerPos = normalizedPositionOnAxis(mc.player.getPos()).multiply(mc.player.getPos().multiply(1,0,1).length());
+                        info("Player pos: " + playerPos);
                         pos = positionInDirection(playerPos, targetYaw, currDistance);
                     }
                     else
@@ -220,6 +225,7 @@ public class GrimEfly extends Module {
                         // TODO: Make this better
                         pos = positionInDirection(mc.player.getPos(), targetYaw, currDistance);
                     }
+                    info("Goal set to " + goal);
                     goal = new BlockPos((int)pos.x + baritoneOffset.get().getX(), targetY.get() + baritoneOffset.get().getY(), (int)pos.z + baritoneOffset.get().getZ());
                     currDistance++;
                 }
