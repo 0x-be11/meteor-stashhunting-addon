8f66fb1b9918b1ed7084ef3a1e83a401d97a3950
mtdaly <mtdaly@ncsu.edu>
2025-04-25 18:06:47 -0400

    Added a mode to OldChunkNotifier to notify you on old chunks perpendicular to the highway.

--- src/main/java/com/stash/hunt/modules/OldChunkNotifier.java
+++ src/main/java/com/stash/hunt/modules/OldChunkNotifier.java
@@ -3,38 +3,88 @@ package com.stash.hunt.modules;
 import com.stash.hunt.Addon;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.Vec3d;
+import xaero.hud.minimap.BuiltInHudModules;
+import xaero.hud.minimap.module.MinimapSession;
+import xaero.hud.minimap.waypoint.set.WaypointSet;
+import xaero.hud.minimap.world.MinimapWorld;
+import xaero.map.mods.SupportMods;
 import xaeroplus.XaeroPlus;
 import xaeroplus.event.ChunkDataEvent;
 import xaeroplus.module.ModuleManager;
 import xaeroplus.module.impl.OldChunks;
 import xaeroplus.module.impl.PaletteNewChunks;
+import xaero.common.minimap.waypoints.Waypoint;
 
-import static com.stash.hunt.Utils.sendWebhook;
+import static com.stash.hunt.Utils.*;
 
 
 public class OldChunkNotifier extends Module {
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    public final Setting<String> webhookLink = sgGeneral.add(new StringSetting.Builder()
-        .name("Webhook Link")
-        .description("A discord webhook link. Looks like this: https://discord.com/api/webhooks/webhookUserId/webHookTokenOrSomething")
-        .defaultValue("")
-        .build()
-    );
-
-    public final Setting<Boolean> ping = sgGeneral.add(new BoolSetting.Builder()
-        .name("Ping")
-        .description("Whether to ping you or not.")
+    private final Setting<Boolean> notifyAnyChunks = sgGeneral.add(new BoolSetting.Builder()
+        .name("Notify Any Chunks")
+        .description("Whether to notify you of any old chunks.")
         .defaultValue(false)
         .build()
     );
 
-    public final Setting<String> discordId = sgGeneral.add(new StringSetting.Builder()
+    private final Setting<Boolean> notifyOffHighway = sgGeneral.add(new BoolSetting.Builder()
+        .name("Notify Trails Off Highway")
+        .description("Whether to notify you of old chunks off the highway.")
+        .defaultValue(false)
+        .build()
+    );
+
+    private final Setting<Double> directionOfTravel = sgGeneral.add(new DoubleSetting.Builder()
+        .name("Direction of Travel")
+        .description("The direction of travel (yaw) in degrees.")
+        .defaultValue(0)
+        .min(-180)
+        .max(180)
+        .visible(notifyOffHighway::get)
+        .build()
+    );
+
+    private final Setting<Double> distanceOffAxis = sgGeneral.add(new DoubleSetting.Builder()
+        .name("Distance Off Axis")
+        .description("The distance in chunks off the axis of movement from the player to check for old chunks.")
+        .defaultValue(13)
+        .sliderRange(0, 15)
+        .visible(notifyOffHighway::get)
+        .build()
+    );
+
+    private final Setting<LogType> logType = sgGeneral.add(new EnumSetting.Builder<LogType>()
+        .name("Log Type")
+        .description("What to do when an old chunk is detected.")
+        .defaultValue(LogType.Marker)
+        .build()
+    );
+
+    private final Setting<String> webhookLink = sgGeneral.add(new StringSetting.Builder()
+        .name("Webhook Link")
+        .description("A discord webhook link. Looks like this: https://discord.com/api/webhooks/webhookUserId/webHookTokenOrSomething")
+        .defaultValue("")
+        .visible(() -> logType.get() == LogType.Webhook || logType.get() == LogType.Both)
+        .build()
+    );
+
+    private final Setting<Boolean> ping = sgGeneral.add(new BoolSetting.Builder()
+        .name("Ping")
+        .description("Whether to ping you or not.")
+        .defaultValue(false)
+        .visible(() -> logType.get() == LogType.Webhook || logType.get() == LogType.Both)
+        .build()
+    );
+
+    private final Setting<String> discordId = sgGeneral.add(new StringSetting.Builder()
         .name("Discord ID")
         .description("Your discord ID")
         .defaultValue("")
-        .visible(ping::get)
+        .visible(() -> ping.get() && (logType.get() == LogType.Webhook || logType.get() == LogType.Both))
         .build()
     );
 
@@ -57,10 +107,11 @@ public class OldChunkNotifier extends Module {
     @net.lenni0451.lambdaevents.EventHandler(priority = -1)
     public void onChunkData(ChunkDataEvent event)
     {
+        if (event.seenChunk()) return;
+
         // avoid 2b2t end loading screen
         if (mc.player.getAbilities().allowFlying) return;
 
-        if (webhookLink.get().isEmpty()) return;
         boolean is119NewChunk = ModuleManager.getModule(PaletteNewChunks.class)
             .isNewChunk(
                 event.chunk().getPos().x,
@@ -77,20 +128,74 @@ public class OldChunkNotifier extends Module {
 
         if (is119NewChunk && !is112OldChunk) return;
 
-        String message = "";
-
-        if (is112OldChunk && !is119NewChunk) {
-            message = "1.12 Followed in 1.19+ Old Chunk Detected";
-        } else if (is112OldChunk && is119NewChunk) {
-            message = "1.12 Unfollowed in 1.19+ Old Chunk Detected";
-        } else {
-            message = "1.19+ Old Chunk Detected";
+        if (notifyAnyChunks.get())
+        {
+            if (logType.get() == LogType.Both || logType.get() == LogType.Marker)
+            {
+                createMapMarker(event.chunk().getPos().x, event.chunk().getPos().z);
+            }
+            if (logType.get() == LogType.Both || logType.get() == LogType.Webhook)
+            {
+                String message = "";
+                if (is112OldChunk && !is119NewChunk) {
+                    message = "1.12 Followed in 1.19+ Old Chunk Detected";
+                } else if (is112OldChunk && is119NewChunk) {
+                    message = "1.12 Unfollowed in 1.19+ Old Chunk Detected";
+                } else {
+                    message = "1.19+ Old Chunk Detected";
+                }
+                String finalMessage = message; // must be final for thread operations
+                // use threads so if a ton of chunks come at once it doesnt lag the game
+                String discordID = discordId.get().isBlank() ? null : discordId.get();
+                new Thread(() -> sendWebhook(webhookLink.get(), "Old Chunk Detected", finalMessage + " at " + mc.player.getPos().toString(), discordID, mc.player.getGameProfile().getName())).start();
+            }
         }
-        String finalMessage = message;
-        // use threads so if a ton of chunks come at once it doesnt lag the game
-        String discordID = discordId.get().isBlank() ? null : discordId.get();
-        new Thread(() -> sendWebhook(webhookLink.get(), "Old Chunk Detected", finalMessage + " at " + mc.player.getPos().toString(), discordID, mc.player.getGameProfile().getName())).start();
 
+        if (notifyOffHighway.get())
+        {
+            ChunkPos chunkPos = event.chunk().getPos();
+            Vec3d direction = yawToDirection(directionOfTravel.get());
+            ChunkPos playerChunkPos = mc.player.getChunkPos();
+            double distance = distancePointToDirection(new Vec3d(chunkPos.x, 0, chunkPos.z), direction, new Vec3d(playerChunkPos.x, 0, playerChunkPos.z));
+            if (distance > distanceOffAxis.get())
+            {
+                if (logType.get() == LogType.Both || logType.get() == LogType.Marker)
+                {
+                    createMapMarker(chunkPos.x, chunkPos.z);
+                }
+                if (logType.get() == LogType.Both || logType.get() == LogType.Webhook)
+                {
+                    new Thread(() -> sendWebhook(webhookLink.get(), "Old Chunk Detected", "Old chunk detected at " + chunkPos.x + " " + chunkPos.z, discordId.get(), mc.player.getGameProfile().getName())).start();
+                }
+            }
+        }
     }
 
+    private void createMapMarker(int x, int z)
+    {
+        MinimapSession minimapSession = BuiltInHudModules.MINIMAP.getCurrentSession();
+        if (minimapSession == null) return;
+        MinimapWorld currentWorld = minimapSession.getWorldManager().getCurrentWorld();
+        if (currentWorld == null) return;
+        WaypointSet waypointSet = currentWorld.getCurrentWaypointSet();
+        if (waypointSet == null) return;
+        Waypoint waypoint = new Waypoint(
+            x * 16,
+            70,
+            z * 16,
+            "Old Chunk",
+            "O",
+            5,
+            0,
+            false);
+        waypointSet.add(waypoint);
+        SupportMods.xaeroMinimap.requestWaypointsRefresh();
+    }
+
+    private enum LogType
+    {
+        Webhook,
+        Marker,
+        Both
+    }
 }
