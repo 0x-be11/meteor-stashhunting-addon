ee04392b5f7ec98b59df3b62042d0b0801884af0
miles352 <mtdaly@ncsu.edu>
2025-05-31 18:43:20 -0400

    Added an auto adjust pitch setting to control the speed in ElytraFlyPlusPlus

--- src/main/java/com/stash/hunt/modules/ElytraFlyPlusPlus.java
+++ src/main/java/com/stash/hunt/modules/ElytraFlyPlusPlus.java
@@ -10,6 +10,7 @@ import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.player.ChestSwap;
+import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.meteorclient.utils.player.FindItemResult;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.orbit.EventHandler;
@@ -51,11 +52,27 @@ public class ElytraFlyPlusPlus extends Module {
         .build()
     );
 
+    private final Setting<Boolean> autoAdjustPitch = sgGeneral.add(new BoolSetting.Builder()
+        .name("Auto Adjust Pitch")
+        .description("Whether to auto adjust your pitch to stay at a set speed")
+        .defaultValue(false)
+        .visible(() -> bounce.get() && lockPitch.get())
+        .build()
+    );
+
     private final Setting<Double> pitch = sgGeneral.add(new DoubleSetting.Builder()
         .name("Pitch")
         .description("The pitch to set when bounce is enabled.")
         .defaultValue(90.0)
-        .visible(() -> bounce.get() && lockPitch.get())
+        .visible(() -> bounce.get() && lockPitch.get() && !autoAdjustPitch.get())
+        .build()
+    );
+
+    private final Setting<Double> speed = sgGeneral.add(new DoubleSetting.Builder()
+        .name("Speed")
+        .description("The speed in blocks per second to keep you at.")
+        .defaultValue(100.0)
+        .visible(() -> bounce.get() && lockPitch.get() && autoAdjustPitch.get())
         .build()
     );
 
@@ -452,7 +469,15 @@ public class ElytraFlyPlusPlus extends Module {
                 }
                 if (lockPitch.get())
                 {
-                    mc.player.setPitch(pitch.get().floatValue());
+                    if (autoAdjustPitch.get())
+                    {
+                        double playerSpeed = Utils.getPlayerSpeed().multiply(1, 0, 1).length();
+                        mc.player.setPitch((float) Math.min(90, Math.max(-90, (speed.get() - playerSpeed) * 5)));
+                    }
+                    else
+                    {
+                        mc.player.setPitch(pitch.get().floatValue());
+                    }
                 }
             }
         }
